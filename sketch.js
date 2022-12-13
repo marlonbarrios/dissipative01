57554ebb6cfbbf8d4a93cd759b970e47458bcd8d
marlonbarrios <marlon@dance-tech.net>
2022-12-13 10:26:13 -0500

    packing circles connected

--- sketch.js
+++ sketch.js
@@ -2,41 +2,91 @@ const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5()
 
+
+var circles = [];
+
+var minRadius = 3;
+var maxRadius = 50;
+
+var showCircle = true;
+var showLine = true;
+
+
+
 const settings = {
   pixelsPerInch: 300,
   p5: true,
   duration: 3,
-  animate: false,
+  animate: true,
   dimensions: [512, 512],
   bleed: 1 / 8,
 };
 
+
 canvasSketch(() => {
+  
+  noFill();
+ 
+  ellipseMode(RADIUS);
+  rectMode(RADIUS);
 
-  angleMode(DEGREES);
-  noStroke();
-
-  return ({
-    playhead,
+  return ({contex,
     width,
     height
   }) => {
-    clear();
-    radialGradient(
-      width / 2, height / 2, width / 2, //Start pX, pY, start circle radius
-      width / 2, height / 2, 1, //End pX, pY, End circle radius
-      color(255, 255, 255, 5), //End color
-      color(0, 0, 0, 200), //Start color
-    );
-    ellipse(width / 2, height / 2, width, height);
+clear();
+
+  // Choose a random or the current mouse position
+  var newX = random(maxRadius, width - maxRadius);
+  var newY = random(maxRadius, height - maxRadius);
+
+  var intersection = false;
+  for (var newR = maxRadius; newR >= minRadius; newR--) {
+    for (var i = 0; i < circles.length; i++) {
+      var d = dist(newX, newY, circles[i].x, circles[i].y);
+      intersection = d < circles[i].r + newR;
+      if (intersection) {
+        break;
+      }
+    }
+    if (!intersection) {
+      circles.push(new Circle(newX, newY, newR));
+      break;
+    }
   }
 
-  function radialGradient(sX, sY, sR, eX, eY, eR, colorS, colorE) {
-    let gradient = drawingContext.createRadialGradient(
-      sX, sY, sR, eX, eY, eR
-    );
-    gradient.addColorStop(0, colorS);
-    gradient.addColorStop(1, colorE);
-    drawingContext.fillStyle = gradient;
+  for (var i = 0; i < circles.length; i++) {
+    if (showLine) {
+      // Try to find an adjacent circle to the current one and draw a connecting line between the two
+      var closestCircle;
+      for (var j = 0; j < circles.length; j++) {
+        var d = dist(circles[i].x, circles[i].y, circles[j].x, circles[j].y);
+        if (d <= circles[i].r + circles[j].r + 1) {
+          closestCircle = circles[j];
+          break;
+        }
+      }
+      if (closestCircle) {
+        stroke(0);
+        strokeWeight(0.95);
+        line(circles[i].x, circles[i].y, closestCircle.x, closestCircle.y);
+      }
+    }
+
+    // Draw the circle itself.
+    if (showCircle) circles[i].draw();
   }
+
+}
+function Circle(x, y, r) {
+  this.x = x;
+  this.y = y;
+  this.r = r;
+
+  Circle.prototype.draw = function() {
+    stroke(0);
+    strokeWeight(1.5);
+    ellipse(this.x, this.y, this.r);
+  };
+}
 }, settings);
